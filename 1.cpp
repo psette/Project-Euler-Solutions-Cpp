8e6860e44bdab2d5464654b96eaddc29522dbb34
psette <psette@usc.edu>
2015-11-11 23:17:25 -0800

    first problem

---
+++ 1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main(){
+   int i=1,sum=0;
+   while(i<1000){
+      if(i%3==0||i%5==0){
+      sum+=i;}
+      i++;
+   }
+   std::cout<<sum<<std::endl;
+   return 1;
+   
+}
