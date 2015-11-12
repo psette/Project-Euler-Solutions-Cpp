2e7bb98adbc68969c63d11ce7344821151e623ff
psette <psette@usc.edu>
2015-11-12 01:13:56 -0800

    second problem

---
+++ 2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+long fib1 = 1;
+long fib2 = 1;
+long result = 0;
+long summed = 0;
+ 
+while (result < 4000000) {
+    if ((result % 2) == 0) {
+        summed += result;
+    }
+ 
+    result = fib1 + fib2;
+    fib2 = fib1;
+    fib1 = result;
+}
+cout<<summed;
+}
