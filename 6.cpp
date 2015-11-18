3ea7708684d8b25f1ed1b72b8a5110ea363309df
psette <psette@usc.edu>
2015-11-18 00:01:49 -0800

    sixth

---
+++ 6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+   int sum_of_s=0;
+   int s_of_sum=0;
+   for(int i=1;i<=100;++i){
+   sum_of_s+=i*i;
+   s_of_sum+=i;  
+   }
+   s_of_sum=s_of_sum*s_of_sum;
+   cout<<s_of_sum-sum_of_s<<endl;
+}
