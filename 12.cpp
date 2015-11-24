94f0e2e57ca0979f7434c9d3256199595f26fdc3
psette <psette@usc.edu>
2015-11-24 12:50:12 -0800

    12

---
+++ 12.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "math.h"
+int count(int n);
+int main(){
+	int num = 0, i = 1;
+	while(count(num)<500){
+		num += i;
+		i++;		
+	}
+	std::cout<<num<<std::endl;	
+}
+int count(int n){
+	int c=0;
+	int sqr = sqrt(n);
+	for( int i = 1; i <= sqr; i++){
+		if(n % i == 0){
+			c += 2;
+		}
+	}
+	return c;
+}
