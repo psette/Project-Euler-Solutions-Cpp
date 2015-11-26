58fae89bffb16f9679705bbb96acc32978bc62ae
psette <psette@usc.edu>
2015-11-25 23:17:14 -0800

    13

--- 13.cpp
+++ 13.cpp
@@ -4,18 +4,18 @@ int main ()
 {	
 	int result[50];
 	int car = 0;
-	string numbers[100];
+	std::string numbers[100];
 	for(int i =0;i<100;i++){
 		std::cin>>numbers[i];
 	}
  	for(int i=0;i<50;i++){
- 		result[50-i]=car;
+ 		result[49-i]=car;
  		for(int j=0;j<100;j++){
- 			result[50-i]+= (int)numbers[j].at[50-i];
+ 			result[49-i]+= (int)numbers[j].at(50-i);
  		}
- 		car=result[50-i]/10;
+ 		car=result[49-i]/10;
  	}
-	for (x = 0; x < 50; x++){
+	for (int i=0; i<50; i++){
 		std::cout <<result[i]%10<<std::endl;
 	}	
 	return 0;
