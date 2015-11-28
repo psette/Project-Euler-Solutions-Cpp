4eaffb9a4a6441df66cd2ce248cf5aebb1fb5b14
psette <psette@usc.edu>
2015-11-28 10:58:11 -0800

    16

--- 16.cpp
+++ 16.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
-#include <cmath>
+#include <vector>
 int main(){
-	int result=0;
-	unsigned long long num=(int)pow(2,1000);
-	std::cout<<num; 
-	while (num > 0) {
-    	result+=num%10;
-    	num/= 10;
+	std::vector<int> bignum;
+	bignum.push_back(2);
+	int carry=0, size=bignum.size(), result=0;
+	for(int j=1;j<=1000;j++){
+		size=bignum.size();
+		for(int i=0;i<size;i++){
+			if(bignum[i]*2+carry>=10){
+				bignum[i]=bignum[i]*2-10;
+				carry=1;
+				if(i==size-1){
+					bignum.push_back(1);
+				}
+				continue;
+			}
+				bignum[i]=bignum[i]*2+carry;
+				carry=0;
+		}
+	}
+	for(int i=0;i<bignum.size();i++){
+		result+=bignum[i];
 	}
 	std::cout<<result<<std::endl;
-	return 0;
 }
+	/*
+	1511270012867
+	*/
