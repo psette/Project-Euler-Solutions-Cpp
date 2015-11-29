d27e082f5abdb884f88969398d3522ff0f6ca07e
psette <psette@usc.edu>
2015-11-28 18:23:00 -0800

    16 revised

--- 16.cpp
+++ 16.cpp
@@ -3,12 +3,13 @@
 int main(){
 	std::vector<int> bignum;
 	bignum.push_back(2);
-	int carry=0, size=bignum.size(), result=0;
-	for(int j=1;j<=1000;j++){
+	int carry=0, size=bignum.size(), result;
+	for(int j=1;j<1000;j++){
 		size=bignum.size();
+		carry=0;
 		for(int i=0;i<size;i++){
-			if(bignum[i]*2+carry>=10){
-				bignum[i]=bignum[i]*2-10;
+			if(bignum[i]*2+carry>9){
+				bignum[i]=bignum[i]*2+carry-10;
 				carry=1;
 				if(i==size-1){
 					bignum.push_back(1);
@@ -19,11 +20,8 @@ int main(){
 				carry=0;
 		}
 	}
-	for(int i=0;i<bignum.size();i++){
+	for(int i=bignum.size()-1;i>=0;i--){	
 		result+=bignum[i];
-	}
+ 	}
 	std::cout<<result<<std::endl;
 }
-	/*
-	1511270012867
-	*/
