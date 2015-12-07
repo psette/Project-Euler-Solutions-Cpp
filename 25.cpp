b4345c77764c765a7d0178d99ddb22efdbc8a2bc
psette <psette@usc.edu>
2015-12-07 13:04:57 -0800

    25

---
+++ 25.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+#include <cmath>
+int main(){
+	double length=0;
+	int n = 1;
+	while( length < 999){
+		n++;
+		length =  n * log10( 0.5 + pow(5,0.5) / 2) - log10(5) / 2;
+	}
+	std::cout<< n <<std::endl;
+	return 0;
+}
