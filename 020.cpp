7910d25bdcb73e3ba57056026cde0649173496d0
psette <psette@usc.edu>
2015-12-26 02:30:01 -0800

    20

--- 020.cpp
+++ 020.cpp
@@ -1,41 +1,21 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 int main(){
-	std::vector<int> bignum;
-	bignum.push_back(1)
-	int counter = 1,carry = 0,answer = 0;
-	std::vector<int> ones, tens, result;
-	for (int i = 0; i < 10; i++){
-		for (int j = 0; j < 10; j++){
-			int temp1 = 0, temp2 = 0, size = bignum.size();
-			for (int k = 0; k < size; k++){
-				temp1 = bignum[k] * j;
-				temp2 = bignum[k] * i;
-				while (temp1!=0){
-	    			ones.push_back(temp1 % 10); 
-	    			temp1 = i / 10;
-				}
-				while (temp2 != 0){
-	    			tens.push_back(temp2 % 10); 
-	    			 temp2 = i / 10;
-				}
-				ones.reverse();
-				tens.reverse();
-				tens.push_back(0);
-				if (ones.size() > tens.size()){
-					for (int i = 0; i < ones.size; i++){
-						(i < tens.size() ) ? ones[i] += tens[i] : ones[i] = ones[i];
-					}					
-				}
-				else{
-					for (int i = 0; i < tens.size; i++){
-						(i < ones.size() ) ? tens[i] += ones[i] : tens[i] = tens[i];
-					}
-				}
-			}
-		}
-	}
-	std::cout<< result <<std::endl;
-	return 0;
+int temp = 0, carry = 0, num = 0, sum = 0, digits[ 175 ] = { 0 };
+digits[0] = 1;
+for(int i = 1; i < 100; i++) {
+  carry = 0;
+  for (int j = 0;j < 175; j++){
+    temp = digits[j] * i + carry;
+    carry = 0;
+    if (temp > 9){
+      num = temp % 10;
+      carry = temp / 10;      
+    }
+    else	num = temp;
+    digits[j] = num;
+  }
+}
+for (int i = 175 ;i > 0;i--)	sum += digits[ i - 1 ];
+std::cout<< sum <<std::endl;
+return 0;
 }
