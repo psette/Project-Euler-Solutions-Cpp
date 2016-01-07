36c15d6d1c2d0bc738187424af71af8dd88432b3
psette <psette@usc.edu>
2016-01-06 22:56:24 -0800

    050

--- 050.cpp
+++ 050.cpp
@@ -1,36 +1,27 @@
 #include <iostream>
 #include <vector>
-bool isPrime(std::vector<long> primes, long key, int high, int low = 0){
-	if(low > high)	return false;
-	int mid = (low + high) / 2;
-	if( primes[mid] > key) return isPrime(primes, key, low, mid - 1);
-	if( primes[mid] < key) return isPrime(primes, key, mid + 1, high);
-	if( primes[mid] == key) return true;
-}
-bool isPrime(long n){
+bool isPrime(int n){
 	if( n == 2) return true;
 	if( n % 2 == 0 || n == 1) return false;
-	for(int i = 3; i * i < n; i += 2)
-		if( n % i == 0)	return false;
+	for(int i = 3; i * i <= n; i += 2)	if( n % i == 0)	return false;
 	return true;
 }
 int main(){
-	int conseq_primes = 0;
+	unsigned int conseq_primes = 0, bigPrime = 0;
 	std::vector<long> primes;
-	for(int i = 1; i < 1000000; i++)	if( isPrime( i ) ) primes.push_back( i );
-	long result = 0, *primesum = new long[primes.size() + 1];
-	primesum[0]=0;
-	for(int i = 0; i < primes.size(); i++ )	primesum[i + 1] = primesum[i] + primes[i];
-	for(int i = conseq_primes; i < primes.size(); i++){
-		for(int j = i - (conseq_primes + 1); j >= 0; j--){
-			if( primesum[i] - primesum[j] > primes[primes.size() - 1] )	break;
-			if( isPrime(primes, primesum[i] - primesum[j], primes.size())){
-				conseq_primes = i - j;
-				result = primesum[i] - primesum[j];
+	for(int i = 2; i < 1000000; i++)
+		if(isPrime( i )) primes.push_back( i++ );
+	for(int i = 0; i < primes.size(); i++) {
+		unsigned int sum = 0, j = i;
+		while(sum < primes[primes.size() - 1]) {
+			sum += primes[j++];
+			if ( sum > bigPrime && j - i > conseq_primes && isPrime(sum) ) {
+				bigPrime = sum;
+				conseq_primes = j - i;
+
 			}
 		}
 	}
-	std::cout<< result <<std::endl;
-	delete [] primesum;
+	std::cout<< bigPrime <<std::endl;
 	return 0;
 }
